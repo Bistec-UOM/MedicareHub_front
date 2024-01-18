0e9bb499b155944747b3f06ae23a707a7876cc4d
Dhammika-Mahendra <wmdhammikamahendra@gmail.com>
2024-01-18 15:28:12 +0530

    initial set up 2

--- front/src/App.js
+++ front/src/App.js
@@ -1,8 +1,24 @@
+import {Routes, Route,BrowserRouter} from "react-router-dom";
+import Admin from "./pages/Admin";
+import Doctor from "./pages/Doctor";
+import Pharmacy from "./pages/Pharmacy";
+import Receptionist from "./pages/Receptionist";
+import Lab from "./pages/Lab";
+import Log from "./pages/Log";
 
 function App() {
   return (
     <div className="App">
-      <h1>Thi is head</h1>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Log></Log>}></Route>
+      <Route path='admin' element={<Admin></Admin>}></Route>
+      <Route path='recep' element={<Receptionist></Receptionist>}></Route>
+      <Route path='doct' element={<Doctor></Doctor>}></Route>
+      <Route path='pharm' element={<Pharmacy></Pharmacy>}></Route>
+      <Route path='lab' element={<Lab></Lab>}></Route>
+    </Routes>
+    </BrowserRouter>
     </div>
   );
 }
