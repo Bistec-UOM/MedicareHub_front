aa0b52871ca6b92499a346ef1be06d519c937cce
Ramosh99 <einfetch@gmail.com>
2024-01-18 15:48:54 +0530

    updated

--- front/src/App.js
+++ front/src/App.js
@@ -1,24 +1,25 @@
-import {Routes, Route,BrowserRouter} from "react-router-dom";
 import Admin from "./pages/Admin";
 import Doctor from "./pages/Doctor";
 import Pharmacy from "./pages/Pharmacy";
 import Receptionist from "./pages/Receptionist";
 import Lab from "./pages/Lab";
 import Log from "./pages/Log";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-    <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<Log></Log>}></Route>
+      <BrowserRouter>
+        <Routes>
+        <Route path='/' element={<Log></Log>}></Route>
       <Route path='admin' element={<Admin></Admin>}></Route>
       <Route path='recep' element={<Receptionist></Receptionist>}></Route>
       <Route path='doct' element={<Doctor></Doctor>}></Route>
       <Route path='pharm' element={<Pharmacy></Pharmacy>}></Route>
       <Route path='lab' element={<Lab></Lab>}></Route>
-    </Routes>
-    </BrowserRouter>
+
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
