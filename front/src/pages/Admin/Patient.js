491c2edc892341c7c4ea218a9c55300408760547
Ramosh99 <einfetch@gmail.com>
2024-01-21 22:17:29 +0530

    updated

--- front/src/pages/Admin/Patient.js
+++ front/src/pages/Admin/Patient.js
@@ -1,7 +1,319 @@
-export default function Patient() {
-    return(
-        <div>
-            <h1>Patient</h1>
-        </div>
-    );
+import * as React from "react";
+import Paper from "@mui/material/Paper";
+import InputBase from "@mui/material/InputBase";
+import Divider from "@mui/material/Divider";
+import IconButton from "@mui/material/IconButton";
+import SearchIcon from "@mui/icons-material/Search";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import { useState } from "react";
+import {Checkbox,Dialog,DialogActions,DialogContent,DialogTitle,FormControl,FormControlLabel,FormGroup,Grid,InputLabel,MenuItem,Select,TextField,Typography,} from "@mui/material";
+import { Button } from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
+
+function createData(Id,fullName,name,NIC,address,contactNumber,emailAddress,age,gender
+) {
+  return {Id,fullName,name,NIC,address,contactNumber,emailAddress,age,gender,
+  };
 }
+const rows = [
+  createData(1,"chamath palliyaguruge","chamath",159,6.0,24,4,30,"male"),
+  createData(2, "wimal kostha", "wimal", 237, 9.0, 37, 4.3, 30, "female"),
+  createData(3, "kumara sangakkara", "kumara", 262, 16.0, 24, 6.0, 30, "male"),
+  createData(4,"pathirana saman","pathirana",305,3.7,67,4.3,30,"female"),
+  createData(5, "lavu kanush", "kanush", 356, 16.0, 49, 3.9, 30, "male"),
+];
+
+function Patient() {
+  const [open, setOpen] = React.useState(false);
+  const [editOpen, setEditOpen] = React.useState(false);
+  const [selectedPaper, setSelectedPaper] = useState(null);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleEditSave = () => {
+    // Handle saving edited data here
+    console.log("Edited data:", selectedPaper);
+    setEditOpen(false);
+  };
+
+  const handleEditClickOpen = (row) => {
+    setSelectedPaper(row);
+    setEditOpen(true);
+  };
+
+  const handleEditClose = () => {
+    setSelectedPaper(null);
+    setEditOpen(false);
+  };
+
+  const handleInputChange = (field, value) => {
+    setSelectedPaper({
+      ...selectedPaper,
+      [field]: value,
+    });
+  };
+
+  return (
+    <div>
+
+      {/* search bar */}
+      <Grid sx={{ display: "flex", justifyContent: "space-between" }}>
+        <Paper
+          component="form"
+          sx={{
+            p: "2px 4px",
+            display: "flex",
+            alignItems: "center",
+            width: "40vh",
+            borderRadius: "20px",
+            boxShadow: 3,
+          }}
+        >
+          <InputBase sx={{ ml: 3, flex: 1 }} placeholder="Search Patient" />
+          <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
+          <IconButton type="button" sx={{ p: "10px" }} aria-label="search">
+            <SearchIcon />
+          </IconButton>
+        </Paper>
+
+        {/* adding patient */}
+        <Button
+          variant="contained"
+          size="small"
+          sx={{
+            backgroundColor: "rgb(121, 204, 190)",
+            width: "10vh",
+            height: "5vh",
+            fontWeight: "bolder",
+          }}
+          onClick={handleClickOpen}
+        >
+          Add
+        </Button>
+      </Grid>
+
+
+      <Grid sx={{display:'flex',justifyContent:'space-around', m:2,width:'90vh'}}>
+{/* fill box */}
+<FormControlLabel
+            control={
+                // <Checkbox checked={gilad} onChange={handleChange} name="gilad" />
+                <Checkbox    sx={{
+                  color: 'rgb(121, 204, 190)',
+                  '&.Mui-checked': {
+                    color: 'rgb(121, 204, 190)',
+                  },
+                }}/>
+            }
+            label="Name"
+          />
+          <FormControlLabel
+            control={
+              <Checkbox    sx={{
+                color: 'rgb(121, 204, 190)',
+                '&.Mui-checked': {
+                  color: 'rgb(121, 204, 190)',
+                },
+              }}/>
+            }
+            label="Address"
+          />
+          <FormControlLabel
+            control={
+              <Checkbox    sx={{
+                color: 'rgb(121, 204, 190)',
+                '&.Mui-checked': {
+                  color: 'rgb(121, 204, 190)',
+                },
+              }}/>
+            }
+            label="Telephone"
+          />
+          <FormControlLabel
+            control={
+              <Checkbox    sx={{
+                color: 'rgb(121, 204, 190)',
+                '&.Mui-checked': {
+                  color: 'rgb(121, 204, 190)',
+                },
+              }}/>
+            }
+            label="ID"
+          />
+          
+        </Grid>
+      <Grid>
+        {/* data adding popup */}
+        <Dialog open={editOpen} onClose={handleEditClose}>
+          <DialogTitle
+            sx={{
+              backgroundColor: "rgb(222, 244, 242)",
+              display: "flex",
+              justifyContent: "space-between",
+            }}
+          >
+            Edit Doctor
+            <CloseIcon onClick={handleEditClose} />
+          </DialogTitle>
+          <DialogContent>
+            <TextField
+              label="Full Name"
+              fullWidth
+              margin="dense"
+              value={selectedPaper ? selectedPaper.name : ""}
+              onChange={(e) => handleInputChange("name", e.target.value)}
+            />
+
+            <FormControl margin="normal">
+              <InputLabel id="gender-label">Gender</InputLabel>
+              <Select
+                labelId="gender-label"
+                id="gender"
+                value={selectedPaper ? selectedPaper.gender : ""}
+                onChange={(e) => handleInputChange("gender", e.target.value)}
+                label="Gender"
+              >
+                <MenuItem value="Male">Male</MenuItem>
+                <MenuItem value="Female">Female</MenuItem>
+              </Select>
+            </FormControl>
+          </DialogContent>
+          <DialogActions>
+            <Button
+              onClick={handleEditSave}
+              variant="contained"
+              sx={{ backgroundColor: "rgb(121, 204, 190)", m: 2 }}
+            >
+              Save
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </Grid>
+      <Grid>
+        {rows.map((row) => (
+          <Paper
+            key={row.Id}
+            sx={{
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+              marginBottom: "10px",
+              padding: 2,
+              boxShadow: 2,
+              borderRadius:'12px'
+            }}
+            onClick={() => handleEditClickOpen(row)} // Pass the row to handleEditClickOpen
+          >
+            <Typography sx={{ flex: 1 }}>{row.fullName}</Typography>
+            <Typography sx={{ flex: 1 }}>{row.NIC}</Typography>
+            <Typography sx={{ flex: 1 }}>{row.gender}</Typography>
+            <Typography sx={{ flex: 1 }}>{row.emailAddress}</Typography>
+          </Paper>
+        ))}
+      </Grid>
+
+      {/* data editing */}
+      <Grid>
+        <Dialog open={editOpen} onClose={handleEditClose}>
+          <DialogTitle
+            sx={{
+              backgroundColor: "rgb(222, 244, 242)",
+              display: "flex",
+              justifyContent: "space-between",
+            }}
+          >
+            Edit Doctor
+            <CloseIcon onClick={handleEditClose} />
+          </DialogTitle>
+          <DialogContent>
+            <TextField
+              label="Full Name"
+              fullWidth
+              margin="dense"
+              value={selectedPaper ? selectedPaper.fullName : ""}
+              onChange={(e) => handleInputChange("fullName", e.target.value)}
+            />
+            <TextField
+              label="Name"
+              fullWidth
+              margin="dense"
+              value={selectedPaper ? selectedPaper.name : ""}
+              onChange={(e) => handleInputChange("name", e.target.value)}
+            />
+            <TextField
+              label="NIC"
+              fullWidth
+              margin="dense"
+              value={selectedPaper ? selectedPaper.NIC : ""}
+              onChange={(e) => handleInputChange("protein", e.target.value)}
+            />
+            <TextField
+              label="Address"
+              fullWidth
+              margin="dense"
+              value={selectedPaper ? selectedPaper.address : ""}
+              onChange={(e) => handleInputChange("carbs", e.target.value)}
+            />
+            <TextField
+              label="Contact Number"
+              fullWidth
+              margin="dense"
+              value={selectedPaper ? selectedPaper.contactNumber : ""}
+              onChange={(e) => handleInputChange("name", e.target.value)}
+            />
+            <TextField
+              label="Email"
+              fullWidth
+              margin="dense"
+              value={selectedPaper ? selectedPaper.emailAddress : ""}
+              onChange={(e) => handleInputChange("name", e.target.value)}
+            />
+            <TextField
+              label="Age"
+              fullWidth
+              margin="dense"
+              value={selectedPaper ? selectedPaper.age : ""}
+              onChange={(e) => handleInputChange("name", e.target.value)}
+            />
+
+            <FormControl margin="normal" sx={{ width: "15vh" }}>
+              <InputLabel id="gender-label">Gender</InputLabel>
+              <Select
+                labelId="gender-label"
+                id="gender"
+                value={selectedPaper ? selectedPaper.gender : ""}
+                onChange={(e) => handleInputChange("gender", e.target.value)}
+                label="Gender"
+              >
+                <MenuItem value="Male">Male</MenuItem>
+                <MenuItem value="Female">Female</MenuItem>
+              </Select>
+            </FormControl>
+          </DialogContent>
+          <DialogActions>
+            <Button
+              onClick={handleEditSave}
+              variant="contained"
+              sx={{ backgroundColor: "rgb(121, 204, 190)", m: 2 }}
+            >
+              Save
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </Grid>
+    </div>
+  );
+}
+
+export default Patient;
--- front/src/pages/Admin/Staff.js
+++ front/src/pages/Admin/Staff.js
@@ -1,20 +1,34 @@
-import { Paper, Typography, Button, Dialog, DialogTitle, DialogContent, DialogActions, TextField, FormControl, InputLabel, Select, MenuItem } from "@mui/material";
-import * as React from 'react';
+import {
+  Paper,
+  Typography,
+  Button,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  TextField,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+} from "@mui/material";
+import * as React from "react";
+import CloseIcon from "@mui/icons-material/Close";
 
 export default function Staff() {
   const [open, setOpen] = React.useState(false);
   const [editOpen, setEditOpen] = React.useState(false);
   const [formData, setFormData] = React.useState({
-    fullName: 'Dr.Amal Rathnayake',
-    usualName: 'amal',
-    nic: '992941687',
-    address: 'japan',
-    degree:'MBBS',
-    speciality:'Cardiologist',
-    contactNumber: 't487',
-    emailAddress: 'bcw',
-    age: '34',
-    gender: 'male',
+    fullName: "Dr.Amal Rathnayake",
+    usualName: "amal",
+    nic: "992941687",
+    address: "japan",
+    degree: "MBBS",
+    speciality: "Cardiologist",
+    contactNumber: "t487",
+    emailAddress: "bcw",
+    age: "34",
+    gender: "male",
   });
 
   const handleClickOpen = () => {
@@ -51,28 +65,30 @@ export default function Staff() {
       <Paper
         sx={{
           m: 1,
-          backgroundColor: 'rgb(59, 135, 122)',
-          color: 'white',
-          margin: '10 auto',
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'space-between',
+          backgroundColor: "rgb(59, 135, 122)",
+          color: "white",
+          margin: "10 auto",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
           paddingLeft: 2,
           paddingRight: 2,
         }}
-        onClick={handleEditClickOpen}
       >
-        <Typography variant="h5" sx={{ paddingTop: 0.75, paddingBottom: 0.75, fontWeight: 'bolder' }}>
+        <Typography
+          variant="h5"
+          sx={{ paddingTop: 0.75, paddingBottom: 0.75, fontWeight: "bolder" }}
+        >
           Doctors
         </Typography>
         <Button
           variant="contained"
           size="small"
           sx={{
-            backgroundColor: 'rgb(121, 204, 190)',
-            paddingLeft: '1rem',
-            paddingRight: '1rem',
-            fontWeight: 'bolder',
+            backgroundColor: "rgb(121, 204, 190)",
+            paddingLeft: "1rem",
+            paddingRight: "1rem",
+            fontWeight: "bolder",
           }}
           onClick={handleClickOpen}
         >
@@ -80,75 +96,123 @@ export default function Staff() {
         </Button>
       </Paper>
 
+      {/* data adding */}
+
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle
+          sx={{
+            backgroundColor: "rgb(222, 244, 242)",
+            display: "flex",
+            justifyContent: "space-between",
+          }}
+        >
+          Add Doctor
+          <CloseIcon onClick={handleClose} />
+        </DialogTitle>
+        <DialogContent>
+          {/* Add form fields or other content here */}
+          <TextField label="Name" fullWidth sx={{ mb: 1, mt: 3 }} />
+          <TextField label="Usual Name" sx={{ mb: 1 }} />
+          <TextField label="NIC" sx={{ ml: 4, mb: 1 }} />
+          <TextField label="Address" fullWidth sx={{ mb: 1 }} />
+          <TextField label="Contact Number" sx={{ mb: 1 }} />
+          <TextField label="Degree" sx={{ ml: 4, mb: 1 }} />
+          <TextField label="Specialty" fullWidth sx={{ mb: 1 }} />
+          <TextField label="E-mail" fullWidth sx={{ mb: 1 }} />
+          <TextField label="Age" sx={{ mb: 1 }} />
+          <TextField label="Gender" sx={{ ml: 4, mb: 1 }} />
+          {/* Add more fields as needed */}
+        </DialogContent>
+        <DialogActions>
+          <Button
+            onClick={handleEditSave}
+            variant="contained"
+            sx={{ backgroundColor: "rgb(121, 204, 190)", m: 2 }}
+          >
+            Add
+          </Button>
+        </DialogActions>
+      </Dialog>
+
+      {/* data editing */}
       <Dialog open={editOpen} onClose={handleEditClose}>
-        <DialogTitle sx={{ textAlign: 'center' }}>Edit Doctor</DialogTitle>
+        <DialogTitle
+          sx={{
+            backgroundColor: "rgb(222, 244, 242)",
+            display: "flex",
+            justifyContent: "space-between",
+          }}
+        >
+          Edit Doctor
+          <CloseIcon onClick={handleEditClose} />
+        </DialogTitle>
         <DialogContent>
           <TextField
             label="Full Name"
             fullWidth
             margin="dense"
             value={formData.fullName}
-            onChange={(e) => handleInputChange('fullName', e.target.value)}
+            onChange={(e) => handleInputChange("fullName", e.target.value)}
           />
           <TextField
             label="Usual Name"
             margin="normal"
             value={formData.usualName}
-            onChange={(e) => handleInputChange('usualName', e.target.value)}
+            onChange={(e) => handleInputChange("usualName", e.target.value)}
           />
           <TextField
             label="NIC"
             margin="normal"
             sx={{ ml: 4 }}
             value={formData.nic}
-            onChange={(e) => handleInputChange('nic', e.target.value)}
+            onChange={(e) => handleInputChange("nic", e.target.value)}
           />
           <TextField
             label="Address"
             fullWidth
             margin="normal"
             value={formData.address}
-            onChange={(e) => handleInputChange('address', e.target.value)}
+            onChange={(e) => handleInputChange("address", e.target.value)}
           />
           <TextField
             label="Contact Number"
             margin="normal"
             value={formData.contactNumber}
-            onChange={(e) => handleInputChange('contactNumber', e.target.value)}
+            onChange={(e) => handleInputChange("contactNumber", e.target.value)}
           />
           <TextField
             label="Degree"
             margin="normal"
             sx={{ ml: 4 }}
             value={formData.degree}
-            onChange={(e) => handleInputChange('degree', e.target.value)}
+            onChange={(e) => handleInputChange("degree", e.target.value)}
           />
           <TextField
             label="Speciality"
             margin="normal"
             value={formData.speciality}
-            onChange={(e) => handleInputChange('speciality', e.target.value)}
+            onChange={(e) => handleInputChange("speciality", e.target.value)}
           />
           <TextField
             label="Email Address"
             fullWidth
             margin="normal"
             value={formData.emailAddress}
-            onChange={(e) => handleInputChange('emailAddress', e.target.value)}
+            onChange={(e) => handleInputChange("emailAddress", e.target.value)}
           />
           <TextField
             label="Age"
             margin="normal"
             value={formData.age}
-            onChange={(e) => handleInputChange('age', e.target.value)}
+            onChange={(e) => handleInputChange("age", e.target.value)}
           />
-          <FormControl margin="normal">
+          <FormControl margin="normal" sx={{ width: "15vh",ml:4 }}>
             <InputLabel id="gender-label">Gender</InputLabel>
             <Select
               labelId="gender-label"
               id="gender"
               value={formData.gender}
-              onChange={(e) => handleInputChange('gender', e.target.value)}
+              onChange={(e) => handleInputChange("gender", e.target.value)}
               label="Gender"
             >
               <MenuItem value="Male">Male</MenuItem>
@@ -157,11 +221,12 @@ export default function Staff() {
           </FormControl>
         </DialogContent>
         <DialogActions>
-          <Button onClick={handleEditSave} variant="contained" sx={{ backgroundColor: 'rgb(121, 204, 190)' }}>
+          <Button
+            onClick={handleEditSave}
+            variant="contained"
+            sx={{ backgroundColor: "rgb(121, 204, 190)", m: 2 }}
+          >
             Save
-          </Button>
-          <Button onClick={handleEditClose} variant="contained" sx={{ backgroundColor: 'red' }}>
-            Cancel
           </Button>
         </DialogActions>
       </Dialog>
@@ -170,21 +235,31 @@ export default function Staff() {
       <Paper
         sx={{
           mt: 3,
-          display: 'flex',
-          flexDirection: 'column', // Set to 'column' for vertical display
+          display: "flex",
+          flexDirection: "column", // Set to 'column' for vertical display
           paddingLeft: 2,
           paddingRight: 2,
         }}
         onClick={handleEditClickOpen} // Open the edit window when clicking on the paper
       >
         <Typography variant="h6" sx={{ paddingTop: 0.75 }}>
-          Dr.Amal Rathnayake
+          {formData.fullName}
         </Typography>
-        <Typography variant="h10" sx={{ fontSize: 10, color: 'rgb(186, 177, 177)' }}>
-          MBBS
+        <Typography
+          variant="h10"
+          sx={{ fontSize: 10, color: "rgb(186, 177, 177)" }}
+        >
+          {formData.degree}
         </Typography>
-        <Typography variant="h10" sx={{ fontSize: 10, paddingBottom: 0.75, color: 'rgb(186, 177, 177)' }}>
-          Consultant Neurologist
+        <Typography
+          variant="h10"
+          sx={{
+            fontSize: 10,
+            paddingBottom: 0.75,
+            color: "rgb(186, 177, 177)",
+          }}
+        >
+          {formData.speciality}
         </Typography>
       </Paper>
     </div>
