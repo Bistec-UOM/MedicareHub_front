cb686f2db8d4a6c16c652abd6b2adbefc9e983d1
Chathuminipamodya <ies.22.chathumini@gmail.com>
2024-01-23 18:48:15 +0530

    init pharmacy

--- front/src/pages/Pharmacy.js
+++ front/src/pages/Pharmacy.js
@@ -1,8 +1,41 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import {SidebarContainer,SidebarTop,SidebarList} from '../components/sidebar/Sidebar'
+import { CustomScroll } from '../components/CustomScroll';
+import Navbar from '../components/navbar/Navbar'
+import { Grid,Card, Typography } from '@mui/material'
+import { Sideunit_Patient } from '../components/sidebar/Sideunits';
 
 export default function Pharmacy() {
+
+  useEffect(()=>{
+    document.body.style.margin = '0';
+
+   },[]) 
+  
   return (
-    <div>Pharmacy</div>
+    <CustomScroll>
+    <Navbar></Navbar>
+
+    <Grid container spacing={0} sx={{paddingTop:'64px',height:'100vh'}}>
+      <Grid item xs={3} style={{height:'100%'}}>
+        <SidebarContainer>
+          <SidebarTop>
+
+          </SidebarTop>
+          <SidebarList>
+            
+          </SidebarList>
+        </SidebarContainer>
+      </Grid>
+
+      <Grid item xs={9} style={{height:'100%',overflowY:'scroll'}}>
+     
+      </Grid>
+
+
+    </Grid>
+
+  </CustomScroll>
   )
 }
 
---
+++ front/src/pages/Pharmacy_drugstore.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Pharmacy_drugstore() {
+  return (
+    <div>Pharmacy_drugstore</div>
+  )
+}
