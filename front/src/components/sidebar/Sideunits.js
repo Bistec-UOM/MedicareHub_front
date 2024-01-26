786e7c0d1253cc99ea8639c176d9ff3b2676bc02
Chathura Ishara <112225216+ChathuraIshara@users.noreply.github.com>
2024-01-26 23:06:18 +0530

    recep sidebars structure changed

--- front/src/components/sidebar/Sideunits.js
+++ front/src/components/sidebar/Sideunits.js
@@ -14,8 +14,6 @@ function Sideunit_Doctor({ name, title ,selectedTab,index}) {
 
   };
 
- // console.log()
-
   return (
     <Box
       onClick={handleClick}
@@ -43,47 +41,6 @@ function Sideunit_Doctor({ name, title ,selectedTab,index}) {
   );
 }
 
-<<<<<<< HEAD
-function Sideunit_Patient({ name, status, time }) {
-  return (
-    <div style={{ width: "90%", backgroundColor: "beige" }}>
-      <Typography variant="h6">{name}</Typography>
-      <div>
-        <Chip>{time}</Chip>
-        {status == "done" || status == "pending" ? (
-          <CheckIcon
-            color={status == "done" ? "success" : "warning"}
-          ></CheckIcon>
-        ) : (
-          ""
-        )}
-      </div>
-    </div>
-  );
-}
-
-function Sideunit_Test({ id, name, test, setSelect }) {
-  return (
-    <div
-      style={{ width: "90%", backgroundColor: "beige", marginTop: "5px" }}
-      onClick={() => setSelect(id)}
-    >
-      <Typography sx={{ fontSize: "16px" }}>{name}</Typography>
-      {test.map((el) => {
-        return (
-          <div
-            key={el}
-            style={{
-              display: "inline",
-              border: "1px solid black",
-              paddingLeft: "4px",
-              paddingLeft: "4px",
-              borderRadius: "15px",
-              marginLeft: "4px",
-            }}
-          >
-            {el}
-=======
 
 function Sideunit_Patient({name,status,time,selected}) {
     return (
@@ -92,15 +49,11 @@ function Sideunit_Patient({name,status,time,selected}) {
           <div style={{display:'flex',alignContent:'space-between'}}>
                   <div style={{display:'inline',border:selected==true?'1px solid white':'1px solid grey',paddingLeft:'4px',paddingRight:'4px',borderRadius:'5px',marginLeft:'4px',color: selected==true?'white':'grey'}}>{time}</div>
                   {status=='done'||status=='pending'?<CheckIcon color={status=='done'?'success':'warning'}></CheckIcon>:''}
->>>>>>> f302131a906bfa62b1d88341bc266b6a53a431c4
           </div>
       </div>
     )
 }
 
-<<<<<<< HEAD
-export { Sideunit_Doctor, Sideunit_Patient, Sideunit_Test };
-=======
 function Sideunit_Test({id,name,test,setSelect,selected}) {
     return (
       <div style={{width:'90%',margin:'3px',cursor:'pointer',backgroundColor: selected==true?'#79CCBE':'',padding:'5px',paddingTop:'0',color: selected==true?'#FFFFFF':'',borderRadius:'5px'}} onClick={()=>setSelect(id)}>
@@ -126,6 +79,4 @@ function Sideunit_Bill({name,time,selected}) {
     </div>
   )
 }
-
 export {Sideunit_Doctor,Sideunit_Patient,Sideunit_Test}
->>>>>>> f302131a906bfa62b1d88341bc266b6a53a431c4
