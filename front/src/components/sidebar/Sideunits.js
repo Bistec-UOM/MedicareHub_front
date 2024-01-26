c025471677671506d3f9bc0661875d9dde08b348
Dhammika-Mahendra <wmdhammikamahendra@gmail.com>
2024-01-26 22:15:32 +0530

    Sideunit conflict correction

--- front/src/components/sidebar/Sideunits.js
+++ front/src/components/sidebar/Sideunits.js
@@ -4,15 +4,43 @@ import { Chip } from '@mui/material'
 import CheckIcon from '@mui/icons-material/Check';
 import Divider from '@mui/material/Divider';
 
-function Sideunit_Doctor({name,title}) {
+function Sideunit_Doctor({ name, title ,selectedTab,index}) {
+
+  const [isSelected,setIsselected]=useState(false)
+
+
+  const handleClick = () => {
+    setIsselected(true)
+
+  };
+
   return (
-    <div style={{width:'90%',margin:'3px',cursor:'pointer',marginLeft:'4px',padding:'5px',paddingTop:'0'}}>
-        <Typography sx={{color:'black'}} variant='h6'>Dr. {name}</Typography>
-        <p style={{color:'#9F9D9D'}}>{title}</p>
-        <Divider variant="middle" />
-    </div>
-  )
+    <Box
+      onClick={handleClick}
+      sx={{
+        borderRadius:'25px',
+        width: "90%", 
+        height: "auto",
+        borderRadius: "8px", 
+        display: "flex", 
+        flexDirection: "column", 
+        marginTop:0,
+       // disabled:true,
+        //alignItems: "center", 
+        padding: "8px", 
+        justifyContent:'flex-start',
+        //backgroundColor: isSelected ? "green" : "transparent",
+      }}
+    >
+      <Typography sx={{ color: selectedTab==index?"white":"black",width:"100%",display:'flex',justifyContent:'left' }}  variant="h6">
+        Dr. {name}
+      </Typography>
+      <Typography style={{color: selectedTab==index?"white":"black",width:"100%",display:'flex',justifyContent:'left'}} variant="body2">{title} </Typography>
+      <Divider variant="middle" sx={{ width: '100%' }} />
+    </Box>
+  );
 }
+
 
 function Sideunit_Patient({name,status,time,selected}) {
     return (
