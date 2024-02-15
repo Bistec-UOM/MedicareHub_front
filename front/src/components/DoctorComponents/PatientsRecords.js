72241d5b6be2278c016e1e61ca483b8bce96eb72
nethmi <nethmieranga20@gmail.com>
2024-02-15 21:48:09 +0530

    doctor page changes

--- front/src/components/DoctorComponents/PatientsRecords.js
+++ front/src/components/DoctorComponents/PatientsRecords.js
@@ -4,7 +4,7 @@ import DialogTitle from '@mui/material/DialogTitle';
 import DialogContent from '@mui/material/DialogContent';
 import CloseIcon from '@mui/icons-material/Close';
 import { Grid, Card, Typography } from '@mui/material';
-import TextField from '@mui/material/TextField';;
+
 export default function PatientsRecords(props) {
   
     const {openPopup,setOpenPopup} = props;
@@ -14,13 +14,34 @@ export default function PatientsRecords(props) {
   };
   return (
     <div>
-      <Dialog open={openPopup} maxWidth="xl">
+      <Dialog open={openPopup} maxWidth="s">
       <CloseIcon onClick={handleClose} style={{ position: 'absolute', right: '8px', top: '8px', cursor: 'pointer' }} />
-        <DialogTitle sx={{ m: 0, p: 2 ,}}>6 days ago</DialogTitle>
+       
         <DialogContent dividers  sx={{ maxHeight: '500px', overflowY: 'auto' }}>
         <Grid container spacing={1} sx={{ marginTop: "5px",}}>
                         <Grid item xs={12}>
+                        <DialogTitle sx={{ m: 0, p: 2 ,}}>3 days ago</DialogTitle>
                             <Card sx={{m:1, backgroundColor: '#0099cc', color: 'white', fontSize: '19px',height:'30px', }}>
+                                <Typography gutterBottom variant="p" sx={{ marginLeft: '10px', }}>name</Typography>
+                                <Typography gutterBottom variant="p" sx={{ marginLeft: '100px', }}>quantity</Typography>
+                                <Typography gutterBottom variant="p" sx={{ marginLeft: '150px', }}>hour</Typography>
+                            </Card>
+                            
+                            <Card sx={{m: 2, backgroundColor: '#0099cc', color: 'white', fontSize: '19px',height:'30px', }}>
+                                <Typography gutterBottom variant="p" sx={{ marginLeft: '10px', }}>name</Typography>
+                                <Typography gutterBottom variant="p" sx={{ marginLeft: '100px', }}>quantity</Typography>
+                                <Typography gutterBottom variant="p" sx={{ marginLeft: '150px', }}>hour</Typography>
+                            </Card>
+                            <Card sx={{m: 2, backgroundColor: '#0099cc', color: 'white', fontSize: '19px',height:'30px', }}>
+                                <Typography gutterBottom variant="p" sx={{ marginLeft: '10px', }}>name</Typography>
+                                <Typography gutterBottom variant="p" sx={{ marginLeft: '100px', }}>quantity</Typography>
+                                <Typography gutterBottom variant="p" sx={{ marginLeft: '150px', }}>hour</Typography>
+                            </Card>
+                            <Card sx={{m: 2, backgroundColor: 'rgb(209, 224, 250)', color: 'blue', fontSize: '20px',height:'200px',width:'600px', }}>
+                                                           
+                              </Card> 
+                              <DialogTitle sx={{ m: 0, p: 2 ,}}>16 days ago</DialogTitle>
+                              <Card sx={{m:1, backgroundColor: '#0099cc', color: 'white', fontSize: '19px',height:'30px', }}>
                                 <Typography gutterBottom variant="p" sx={{ marginLeft: '10px', }}>name</Typography>
                                 <Typography gutterBottom variant="p" sx={{ marginLeft: '100px', }}>quantity</Typography>
                                 <Typography gutterBottom variant="p" sx={{ marginLeft: '150px', }}>hour</Typography>
