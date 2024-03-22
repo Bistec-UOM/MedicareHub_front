7dcfa2d440276ece6c1bea18db3caf4402a0f002
Dhammika-Mahendra <wmdhammikamahendra@gmail.com>
2024-03-22 17:28:26 +0530

    Rendering incoming Lab requests

--- front/src/Services/Lab.js
+++ front/src/Services/Lab.js
@@ -2,7 +2,8 @@ const baseURL='http://localhost:7205'
 
 const endPoints = {
     TEST:'/api/Test',
-    TEMPLATE:'/api/Test/Template'
+    TEMPLATE:'/api/Test/Template',
+    REPORT:'/api/Values/ReportRequest'
   };
   
 export { baseURL,endPoints};
