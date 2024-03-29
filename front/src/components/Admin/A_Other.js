e85e6dcbf5c4ad86dc54d971526b110c2a09f8dd
Ramosh99 <einfetch@gmail.com>
2024-03-29 16:11:05 +0530

    updated

--- front/src/components/Admin/A_Other.js
+++ front/src/components/Admin/A_Other.js
@@ -7,20 +7,34 @@ import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
-
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 function createData(name,peratten, attendance,reason) {
   return { name,peratten, attendance,reason };
 }
 
-const rows = [
-  createData('chamath','80%', 159 ,12),
-  createData('kasun','90%', 237 ,5),
-  createData('wimal','94%', 262 ,12),
-  createData('kanchana','98%', 305 ,2),
-  createData('manula','100%', 356 ,0),
-];
+// const rows = [
+//   createData('chamath','80%', 159 ,12),
+//   createData('kasun','90%', 237 ,5),
+//   createData('wimal','94%', 262 ,12),
+//   createData('kanchana','98%', 305 ,2),
+//   createData('manula','100%', 356 ,0),
+// ];
 
 const AOther = () => {
+  const [rows, setRows] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://localhost:7205/api/Analytic/attendance')
+      .then(response => {
+        console.log(response.data);
+        setRows(response.data); 
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }, []); // Empty dependency array means this effect runs once on mount
+
     return (
         <div>
               <Typography sx={{textAlign:'center',fontWeight:'bolder',fontSize:'20px'}}>Attendance of Staff</Typography>
@@ -29,27 +43,82 @@ const AOther = () => {
         <TableHead>
           <TableRow>
             <TableCell align='center' sx={{ fontWeight: 'bold' }}>Name </TableCell>
-          <TableCell align='center' sx={{ fontWeight: 'bold' }}>Attendance(%)</TableCell>
-          <TableCell align="center" sx={{ fontWeight: 'bold' }}>Attendance</TableCell>
-          <TableCell align="center" sx={{ fontWeight: 'bold' }}>Reasonable Leaves</TableCell>
+          <TableCell align='center' sx={{ fontWeight: 'bold' }}>Attendance</TableCell>
+
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows.map((row) => (
-            <TableRow
-              key={row.name}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
-              <TableCell align='center' component="th" scope="row">
-                {row.name}
-              </TableCell>
-              <TableCell align="center">{row.attendance}</TableCell>
-              <TableCell align="center">{row.peratten}</TableCell>
-              <TableCell align="center">{row.reason}</TableCell>
+        
+        <TableRow>
+          <TableCell align='center' colSpan={2} sx={{backgroundColor:'rgb(244, 244, 244)'}}>Doctor</TableCell>
+          </TableRow>
+  {rows.map((row) => (
+    row.d_at.map((item) => (
+      <TableRow
+        key={item.doctName}
+        sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+      >
+        <TableCell align='center' component="th" scope="row">
+          {item.doctName}
+        </TableCell>
+        <TableCell align="center">{item.count}</TableCell>
 
-            </TableRow>
-          ))}
-        </TableBody>
+      </TableRow>
+    ))
+  ))}
+<TableRow>
+          <TableCell align='center' colSpan={2} sx={{backgroundColor:'rgb(244, 244, 244)'}}>Lab Assistant</TableCell>
+          </TableRow>
+  {rows.map((row) => (
+    row.l_at.map((item) => (
+      <TableRow
+        key={item.labAstName}
+        sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+      >
+        <TableCell align='center' component="th" scope="row">
+          {item.labAstName}
+        </TableCell>
+        <TableCell align="center">{item.count}</TableCell>
+
+      </TableRow>
+    ))
+  ))}
+
+<TableRow>
+          <TableCell align='center' colSpan={2} sx={{backgroundColor:'rgb(244, 244, 244)'}}>Receptionist</TableCell>
+          </TableRow>
+  {rows.map((row) => (
+    row.r_at.map((item) => (
+      <TableRow
+        key={item.recepName}
+        sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+      >
+        <TableCell align='center' component="th" scope="row">
+          {item.recepName}
+        </TableCell>
+        <TableCell align="center">{item.count}</TableCell>
+
+      </TableRow>
+    ))
+  ))}
+          <TableRow>
+          <TableCell align='center' colSpan={2} sx={{backgroundColor:'rgb(244, 244, 244)'}}>Cashiers</TableCell>
+          </TableRow>
+  {rows.map((row) => (
+    row.c_at.map((item) => (
+      <TableRow
+        key={item.cashierName}
+        sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+      >
+        <TableCell align='center' component="th" scope="row">
+          {item.cashierName}
+        </TableCell>
+        <TableCell align="center">{item.count}</TableCell>
+
+      </TableRow>
+    ))
+  ))}
+</TableBody>
       </Table>
       </Paper>
         </div>
--- front/src/components/Admin/View.js
+++ front/src/components/Admin/View.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import IncomeOfDay from './AnalyticsComponents.js/IncomeOfDay';
 
 const View = () => {
     return (
         <div>
-            
+            <IncomeOfDay></IncomeOfDay>
         </div>
     );
 }
